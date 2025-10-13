ae305c89809074c65e988acb73f3107d7ec662d0
yuhenabc <yuhenabc@126.com>
2025-10-13 14:21:06 +0800

    update github action file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://yuhenabc.com',
   integrations: [mdx(), sitemap()],
 })
